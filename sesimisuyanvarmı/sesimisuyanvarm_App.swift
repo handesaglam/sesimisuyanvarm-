14c696d7e97c2fe232389b390c852848fb4ad9a6
Hande SAĞLAM <63313545+handesaglam@users.noreply.github.com>
2023-02-24 19:22:00 +0300

    Initial Commit

---
+++ sesimisuyanvarmı/sesimisuyanvarm_App.swift
@@ -0,0 +1,17 @@
+//
+//  sesimisuyanvarm_App.swift
+//  sesimisuyanvarmı
+//
+//  Created by Hande Sağlam on 24.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct sesimisuyanvarm_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
